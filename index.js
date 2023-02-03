1bffb0047d65c6be874b424601eb8e7d39e144e1
Giancarlo leandro <giancarlosleandro1@gmail.com>
2023-02-02 21:36:30 -0500

    Add put service

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 let notes = [
@@ -24,6 +26,8 @@ let notes = [
     }
 ]
 
+const url = '/api/notes';
+
 const generateId = () => {
     const maxId = notes.length > 0
         ? Math.max(...notes.map(n => n.id))
@@ -35,24 +39,36 @@ app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/notes', (request, response) => {
+app.get(url, (request, response) => {
     response.json(notes)
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get(`${url}/:id`, (request, response) => {
     const id = Number(request.params.id)
     const note = notes.find(note => note.id === id)
     response.json(note ? note : response.status(404).end())
 })
 
-app.delete('/api/notes/:id', (request, response) => {
+app.put(`${url}/:id`, (request, response) => {
+    const id = Number(request.params.id)
+    if (!notes.find(x => x.id === id)) {
+        return response.status(400).json({
+            error: 'not found note'
+        })
+    }
+    const body = request.body
+    notes = notes.map(x => x.id === id ? body : x)
+    response.json(body ? body : response.status(404).end())
+})
+
+app.delete(`${url}/:id`, (request, response) => {
     const id = Number(request.params.id)
     notes = notes.filter(note => note.id !== id)
 
     response.status(204).end()
 })
 
-app.post('/api/notes', (request, response) => {
+app.post(url, (request, response) => {
     const body = request.body
     if (!body.content) {
         return response.status(400).json({
